5efaeec7cfb37337cb7b32cfd05ba8bbe5550145
Griffin Bu <darkfall@gamemastercn.com>
2011-03-08 00:22:55 -0600

    2011/3/07 添加序列化公共接口 调整集成体系, SoraNamedObject -> SoraObject, SoraMemoryBuffer -> SoraMemoryFile, SoraMemoryBuffer -> SoraNamedObject

---
+++ Sora/Serializable.h
@@ -0,0 +1,25 @@
+/*
+ *  Serializable.h
+ *  Sora
+ *
+ *  Created by Griffin Bu on 3/7/11.
+ *  Copyright 2011 Griffin Bu(Project Hoshizor). All rights reserved.
+ *
+ */
+
+#ifndef SORA_SERIALIZABLE_H__
+#define SORA_SERIALIZABLE_H__
+
+namespace sora {
+	
+	class SoraMemoryBuffer;
+	
+	class Serializable {
+	public:
+		virtual void serialize(SoraMemoryBuffer& bufferStream) = 0;
+		virtual void unserialize(SoraMemoryBuffer& bufferStream) = 0;
+	};
+	
+} // namespace sora
+
+#endif
