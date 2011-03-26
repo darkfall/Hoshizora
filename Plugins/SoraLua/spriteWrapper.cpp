b73c2699972afa44bd236ff26e83f25097437828
Griffin Bu <darkfall@gamemastercn.com>
2011-03-25 22:19:57 -0500

    guiwrapper recover&echance, some bug fix

---
+++ Plugins/SoraLua/spriteWrapper.cpp
@@ -0,0 +1,9 @@
+//
+//  spriteWrapper.cpp
+//  Sora
+//
+//  Created by Griffin Bu on 3/25/11.
+//  Copyright 2011 Griffin Bu(Project Hoshizor). All rights reserved.
+//
+
+#include "spriteWrapper.h"
---
+++ Plugins/SoraLua/spriteWrapper.h
@@ -0,0 +1,7 @@
+//
+//  spriteWrapper.h
+//  Sora
+//
+//  Created by Griffin Bu on 3/25/11.
+//  Copyright 2011 Griffin Bu(Project Hoshizor). All rights reserved.
+//
