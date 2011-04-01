7472f2d0fd3cc31b36674ec22eb5ffbb8bf352a4
Griffin Bu <darkfall@gamemastercn.com>
2011-04-02 07:01:29 +0800

    fix compile

---
+++ Sora/uncopyable.h
@@ -0,0 +1,25 @@
+/*
+ *  uncopyable.h
+ *  Sora
+ *
+ *  Created by GriffinBu on 12/18/10.
+ *  Copyright 2010 Griffin Bu(Project L). All rights reserved.
+ *
+ */
+
+
+#ifndef SORA_UNCOPYABLE_H_
+#define SORA_UNCOPYABLE_H_
+
+class uncopyable {
+public:
+    uncopyable() {}
+	virtual ~uncopyable() {}
+	
+private:
+	uncopyable& operator=(const uncopyable&);
+	uncopyable(const uncopyable&);
+};
+
+
+#endif
