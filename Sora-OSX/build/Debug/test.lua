8dd42d86a1755cdf1aab96388cdd96ae962fea06
Griffin Bu <darkfall@gamemastercn.com>
2011-06-27 05:23:01 +0800

    major update

--- Sora-OSX/build/Debug/test.lua
+++ Sora-OSX/build/Debug/test.lua
@@ -1,6 +1,7 @@
 spr = spritelib.createSprite("magicCircle.png");
 spritelib.setScale(spr, 0.5, 0.5);
 spritelib.setPositionss(spr, SoraCore:getScreenWidth()-spritelib.getSpriteWidth(spr), SoraCore:getScreenHeight()-spritelib.getSpriteHeight(spr));
+spritelib.setScale(spr, 1.0, 1.0);
 
 function update(dt)
 
