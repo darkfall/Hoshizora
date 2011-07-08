19708ba31d3691cffa372f1f0e331931a51ac3a7
darkfall <darkfall@gamemastercn.com>
2011-07-08 15:03:38 +0800

    win32 fix

---
+++ Sora/helpers/SoraDllHelper.cpp
@@ -0,0 +1,80 @@
+/*
+ *  SoraDllHelper.cpp
+ *  Sora
+ *
+ *  Created by Griffin Bu on 6/27/11.
+ *  Copyright 2011 Griffin Bu(Project Hoshizor). All rights reserved.
+ *
+ */
+
+#include "SoraDllHelper.h"
+
+#ifndef OS_WIN32
+#include <dlfcn.h>
+#else
+#endif 
+
+#include "../Debug/SoraInternalLogger.h"
+
+#include "../SoraFileUtility.h"
+
+namespace sora {
+	
+	SoraDllHelper::~SoraDllHelper() {
+		DLL_MAP::iterator itDll = mOpenedDll.begin();
+		while(itDll != mOpenedDll.end()) {
+			closedl(itDll->second);
+			++itDll;
+		}
+	}
+	
+	ulong32 SoraDllHelper::getdl(const SoraString& name) {
+		DLL_MAP::iterator itDll = mOpenedDll.find(name);
+		if(itDll != mOpenedDll.end())
+			return itDll->second;
+		return 0;
+	}
+
+	ulong32 SoraDllHelper::opendl(const SoraString& path) {
+		ulong32 result = 0;
+		
+#ifndef OS_WIN32
+		void* h = dlopen(path.c_str(), RTLD_NOW | RTLD_GLOBAL);
+		if(h != NULL)
+			result = (ulong32)h;
+		else
+			DebugPtr->log(dlerror(), LOG_LEVEL_ERROR);
+#else
+		result = (ulong32)LoadLibraryA(path.c_str());
+#endif
+		if(result != 0) {
+			mOpenedDll[SoraFileUtility::getFileName(path)] = result;
+		}
+		return result;
+	}
+	
+	void SoraDllHelper::closedl(ulong32 handle) {
+#ifndef OS_WIN32
+		dlclose((void*)handle);
+#else
+		FreeLibrary((HMODULE)handle);
+#endif
+		DLL_MAP::iterator itDll = mOpenedDll.begin();
+		while(itDll != mOpenedDll.end()) {
+			if(itDll->second == handle) {
+				mOpenedDll.erase(itDll);
+				break;
+			}
+			++itDll;
+		}
+	}
+	
+	void* SoraDllHelper::getProc(ulong32 handle, const SoraString& name) {
+#ifndef OS_WIN32
+		return dlsym((void*)handle, name.c_str());
+#else
+		return (void*)GetProcAddress((HMODULE)handle, name.c_str());
+#endif
+	}
+
+} // namespace sora
---
+++ Sora/helpers/SoraDllHelper.h
@@ -0,0 +1,39 @@
+/*
+ *  SoraDLHelper.h
+ *  Sora
+ *
+ *  Created by Griffin Bu on 6/27/11.
+ *  Copyright 2011 Griffin Bu(Project Hoshizor). All rights reserved.
+ *
+ */
+
+#ifndef SORA_DYNAMIC_LIBRARY_HELPER
+#define SORA_DYNAMIC_LIBRARY_HELPER
+
+#include "../SoraPlatform.h"
+#include "../SoraSingleton.h"
+
+#include <map>
+
+namespace sora {
+	
+	class SoraDllHelper: public SoraSingleton<SoraDllHelper> {
+		friend class SoraSingleton<SoraDllHelper>;
+		
+		SoraDllHelper() {}
+		~SoraDllHelper();
+		
+	public:
+		ulong32 opendl(const SoraString& path);
+		ulong32 getdl(const SoraString& name);
+		void closedl(ulong32);
+		void* getProc(ulong32 handle, const SoraString& name);
+		
+	private:
+		typedef std::map<SoraString, ulong32> DLL_MAP;
+		DLL_MAP mOpenedDll;
+	};
+	
+} // namespace sora
+
+#endif // SORA_DYNAMIC_LIBRARY_HELPER
